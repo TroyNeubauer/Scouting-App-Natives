544b73bfb638efaebe93deac7dd41d693104ce1b
Troy Neubauer <troyneubauer@gmail.com>
2018-10-22 22:20:22 -0400

    Finalized implementations for 3 native methods

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,6 @@
-﻿#include <iostream>
+﻿#include <stdio.h>
 #include <libusb.h>
-#include <jni.h>
-using namespace std;
+#include "Scouting Natives.h"
 
 #ifdef __cplusplus
 FILE iob[] = { *stdin, *stdout, *stderr };
@@ -17,10 +16,19 @@ libusb_device **devs; //pointer to pointer of device, used to retrieve a list of
 libusb_context *ctx = NULL; //a libusb session
 
 int count() {
-	return libusb_get_device_list(ctx, &devs);
+	int count = libusb_get_device_list(ctx, &devs);
+	libusb_free_device_list(devs, 1);
+	return count;
 }
 
-JNIEXPORT jboolean JNICALL Java_com_jt_scoutserver_utils_SystemUtils_hasNewDevices(JNIEnv * env, jclass c) {
+/*
+* Class:     com_jt_scoutserver_utils_SystemUtils
+* Method:    nativeExit
+* Signature: ()V
+*/
+;
+
+JNIEXPORT jboolean JNICALL Java_com_jt_scoutserver_utils_SystemUtils_hasNewDevices(JNIEnv *, jclass) {
 	int newCount = count();
 	if (lastCount != newCount) {
 		lastCount = newCount;
@@ -30,57 +38,63 @@ JNIEXPORT jboolean JNICALL Java_com_jt_scoutserver_utils_SystemUtils_hasNewDevic
 	return JNI_FALSE;
 }
 
-JNIEXPORT int JNICALL Java_com_jt_scoutserver_utils_SystemUtils_nativeInit(JNIEnv * env, jclass c) {
-	int r = libusb_init(&ctx); //initialize a library session
-	if (r < 0) {
-		cout << "Error creating libUSB " << r << endl; //there was an error
+JNIEXPORT jint JNICALL Java_com_jt_scoutserver_utils_SystemUtils_nativeInit(JNIEnv *, jclass) {
+	if (ctx) {
+		printf("[CPP] Native init already called... ctx is set. Returning!\n");
+		fflush(stdout);
 		return 1;
 	}
+	int r = libusb_init(&ctx); //initialize a library session
+	if (r < 0) {
+		printf("Error creating libUSB %i\n"); //there was an error
+		return 1;
+	}
+	printf("[CPP] Initalizing the libUSB library...\n");
+	fflush(stdout);
 	lastCount = count();
 	return 0;
 }
 
 JNIEXPORT void JNICALL Java_com_jt_scoutserver_utils_SystemUtils_nativeExit(JNIEnv * env, jclass c) {
-	libusb_free_device_list(devs, 1); //free the list, unref the devices in it
-	libusb_exit(ctx); //close the session
-	cout << "Closing Native libUSB session" << endl;
+	printf("[CPP] Closing libUSB session\n");
+	fflush(stdout);
+	//TODO commenting this may not be the best idea, but it avoids a nasty error
+	//libusb_exit(ctx); //close the session
 }
 
 void printdev(libusb_device *dev) {
-	libusb_device_descriptor desc;
+	struct libusb_device_descriptor desc;
 	int r = libusb_get_device_descriptor(dev, &desc);
 	if (r < 0) {
-		cout << "failed to get device descriptor" << endl;
+		printf("failed to get device descriptor");
 		return;
 	}
-	cout << "Number of possible configurations: " << (int)desc.bNumConfigurations << "  ";
-	cout << "Device Class: " << (int)desc.bDeviceClass << "  ";
-	cout << std::hex;
-	cout << "VendorID: " << desc.idVendor << "  ";
-	cout << "ProductID: " << desc.idProduct << endl;
-	cout << std::dec;
-	libusb_config_descriptor *config;
+	printf("Number of possible configurations: %i ", (int)desc.bNumConfigurations);
+	printf("Device Class: %i ", (int)desc.bDeviceClass);
+	printf("VendorID: %i ", desc.idVendor);
+	printf("ProductID: %i\n", desc.idProduct);
+	struct libusb_config_descriptor *config;
 	if(libusb_get_config_descriptor(dev, 0, &config) == 0 && config) {
 	
-		cout << "Interfaces: " << (int)config->bNumInterfaces << " ||| ";
-		const libusb_interface *inter;
-		const libusb_interface_descriptor *interdesc;
-		const libusb_endpoint_descriptor *epdesc;
+		printf("Interfaces: %i ||| ", (int)config->bNumInterfaces);
+		const struct libusb_interface *inter;
+		const struct libusb_interface_descriptor *interdesc;
+		const struct libusb_endpoint_descriptor *epdesc;
 		for (int i = 0; i < (int)config->bNumInterfaces; i++) {
 			inter = &config->interface[i];
-			cout << "Number of alternate settings: " << inter->num_altsetting << " | ";
+			printf("Number of alternate settings: %i | ", inter->num_altsetting);
 			for (int j = 0; j < inter->num_altsetting; j++) {
 				interdesc = &inter->altsetting[j];
-				cout << "Interface Number: " << (int)interdesc->bInterfaceNumber << " | ";
-				cout << "Number of endpoints: " << (int)interdesc->bNumEndpoints << " | ";
+				printf("Interface Number: %i | ", (int)interdesc->bInterfaceNumber);
+				printf("Number of endpoints: %i | ", (int)interdesc->bNumEndpoints);
 				for (int k = 0; k < (int)interdesc->bNumEndpoints; k++) {
 					epdesc = &interdesc->endpoint[k];
-					cout << "Descriptor Type: " << (int)epdesc->bDescriptorType << " | ";
-					cout << "EP Address: " << (int)epdesc->bEndpointAddress << " | ";
+					printf("Descriptor Type: %i | ", (int)epdesc->bDescriptorType);
+					printf("EP Address: %i | ", (int)epdesc->bEndpointAddress);
 				}
 			}
 		}
-		cout << endl << endl << endl;
+		printf("\n\n\n");
 		libusb_free_config_descriptor(config);
 	}
 	
